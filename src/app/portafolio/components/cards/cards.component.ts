a1c6289c9734dd721807df3bd91dccf201b26e50
Nicolas Vargas <nicolas.vargas1085@gmail.com>
2022-11-27 20:24:56 -0500

    Projects

--- src/app/portafolio/components/cards/cards.component.ts
+++ src/app/portafolio/components/cards/cards.component.ts
@@ -11,32 +11,32 @@ export class CardsComponent {
 
   contCards : any[] = [
     {
-      img: '../../../../assets/bases de datos.png',
+      img: '../../../../assets/img/bases de datos.png',
       desc: 'Un Crud en Angular con una api rest de php',
       rout: 'pCrudAngular'
     },
     {
-      img: '../../../../assets/Login.jpg',
+      img: '../../../../assets/img/Login.jpg',
       desc: 'Un login conectado a una base de datos con token',
       rout: 'project1'
     },
     {
-      img: '../../../../assets/Gifs-Angular.jpg',
+      img: '../../../../assets/img/Gifs-Angular.jpg',
       desc: 'Una aplicacion en Angular para buscar gifs',
       rout: 'project1'
     },
     {
-      img: '../../../../assets/Mapbox.jpg',
+      img: '../../../../assets/img/Mapbox.jpg',
       desc: 'Un mapa en Angular en tiempo real',
       rout: 'pMapbox'
     },
     {
-      img: '../../../../assets/Paises.jpg',
+      img: '../../../../assets/img/Paises.jpg',
       desc: 'Una aplicacion que muestra datos de paises',
       rout: 'project1'
     },
     {
-      img: '../../../../assets/VideoJuegos.jpg',
+      img: '../../../../assets/img/VideoJuegos.jpg',
       desc: 'Un prototipo de juego realizado en Unity',
       rout: 'project1'
     }
